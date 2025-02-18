4e629b4d2246b7ef9b7e2fcaaeda8a48f51397d4
JasonTinotenda <kufakwatenziyason@gmail.com>
2025-02-18 21:25:53 +0200

    first example

---
+++ day2/first_example.py
@@ -0,0 +1,9 @@
+height = 1.8
+school = "Rita Africa"
+is_student = True
+
+full_name = "Jason Kufakwatenzi"
+age = 25
+print(
+    f" Hi my name is {full_name}, i am {age} years old. I attend {school} and i am {height}m tall."
+) 

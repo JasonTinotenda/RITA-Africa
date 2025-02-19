d6f0b569e5bea098f7b8858916fef1e9b11b962c
JasonTinotenda <kufakwatenziyason@gmail.com>
2025-02-19 14:32:36 +0200

    day 2 assignment

---
+++ day2/day2_assignment.py
@@ -0,0 +1,11 @@
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+area = (length * width)
+perimeter = (2*(length+width))
+
+print(
+    f"Length: {length}m\n"
+    f"Width: {width}m\n"
+    f"Area: {area}m\n"
+    f"Perimeter: {perimeter}m"
+)
